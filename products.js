9228c87eda84f4aa19a1f4c4f48d1a2c45c7161e
Martin Leone <martinnikolaslll@gmail.com>
2024-10-30 11:52:04 -0400

    Fetch product data and display basic information.

---
+++ products.js
@@ -0,0 +1,22 @@
+//Task 2 and 3: Fetch Products from the API Using Fetch and Promises and Display Product Details Dynamically
+
+const productList = document.getElementById('productList');
+
+//Fetches data from the API
+fetch('https://www.course-api.com/javascript-store-products')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(products => {
+        products.forEach(product => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${product.title} - $${product.price}`;
+            productList.appendChild(listItem);
+        });
+
+    })
+    
+
